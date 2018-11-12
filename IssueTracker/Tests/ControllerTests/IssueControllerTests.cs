62166e8dc11fe615c409c56b710853ad5a641a67
Jonathan Wilks <jonathan.wilks@lexisnexis.co.uk>
2018-11-12 17:59:19 +0000

    Unit test stubs created awaiting controller and model definition

---
+++ IssueTracker/Tests/ControllerTests/IssueControllerTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ControllerTests
+{
+    [TestClass]
+    public class IssueControllerTests
+    {
+        [TestMethod]
+        public void CanGetAListOfAllIssues()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void CanGetAListOfSpecificUserIssues()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void CanGetAListOfOpenIssues()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void CanCreateANewIssue()
+        {
+            Assert.Fail();
+        }
+        [TestMethod]
+        public void CanCloseAnIssue()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void CanUpdateAnIssue()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void CanAssignAnIssueToAUser()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ IssueTracker/Tests/ControllerTests/UsersControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ControllerTests
+{
+    [TestClass]
+    public class UsersControllerTests
+    {
+
+        [TestMethod]
+        public void CanGetAListOfUsers()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void CanAddAUser()
+        {
+            Assert.Fail();
+        }
+        [TestMethod]
+        public void CanUpdateAUser()
+        {
+            Assert.Fail();
+        }
+    }
+}
